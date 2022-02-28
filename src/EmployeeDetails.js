37647fb88e376d00c9c74a93012a0d31b39c3518
Prateek-Singh1 <prateeksingh1319@gmail.com>
2022-02-28 17:30:13 +0530

    second commit

--- src/EmployeeDetails.js
+++ src/EmployeeDetails.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import "./EmployeeStyle.css";
 
 const EmployeeDetails = () => {
+  const [flag, setflag] = useState(true);
+
+  const setFlag = () => {
+    setflag(false);
+  };
+
   const [userDetails, setUserDetails] = useState({
     username: "",
     department: "",
@@ -25,69 +31,80 @@ const EmployeeDetails = () => {
     setEmploye([...Employe, newEmployee]);
 
     setUserDetails({ username: "", department: "", rating: "" });
+    setFlag(false);
   };
-
+  const handleback = () => {
+    setflag(true);
+  };
   return (
     <div>
-      <h1>Employees feedback Details</h1>
-      <form action="" onSubmit={handleSubmit}>
-        <br />
+      {flag ? (
+        <form action="" onSubmit={handleSubmit}>
+          <h1>Employees feedback Form</h1>
+          <br />
+          <div>
+            <label name="username">Enter Your Name :</label>
+            <input
+              required
+              type="text"
+              typr="text"
+              autoComplete="off"
+              name="username"
+              id="username"
+              value={userDetails.username}
+              onChange={handleChange}
+            ></input>
+          </div>
+          <br />
+          <div>
+            <label name="department">Department :</label>
+            <input
+              required
+              type="text"
+              typr="text"
+              autoComplete="off"
+              name="department"
+              id="department"
+              value={userDetails.department}
+              onChange={handleChange}
+            ></input>
+          </div>
+          <br />
+          <div>
+            <label name="rating">Rating :</label>
+            <input
+              required
+              type="number"
+              type="number"
+              autoComplete="off"
+              name="rating"
+              id="rating"
+              value={userDetails.rating}
+              onChange={handleChange}
+            ></input>
+          </div>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
         <div>
-          <label name="username">Enter Your Name :</label>
-          <input
-            required
-            type="text"
-            typr="text"
-            autoComplete="off"
-            name="username"
-            id="username"
-            value={userDetails.username}
-            onChange={handleChange}
-          ></input>
+          <h1>Employees feedback Details</h1>
+          {Employe.map((newValues) => {
+            return (
+              <>
+                <div className="showDataStyle">
+                  <p>Name : {newValues.username} | </p>
+                  <p>Department : {newValues.department} |</p>
+                  <p>rating : {newValues.rating} </p>
+                </div>
+              </>
+            );
+          })}
+          <br />
+          <br />
+          <button onClick={handleback}>Back</button>
         </div>
-        <br />
-        <div>
-          <label name="department">Department :</label>
-          <input
-            required
-            type="text"
-            typr="text"
-            autoComplete="off"
-            name="department"
-            id="department"
-            value={userDetails.department}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <br />
-        <div>
-          <label name="rating">Rating :</label>
-          <input
-            required
-            type="number"
-            type="number"
-            autoComplete="off"
-            name="rating"
-            id="rating"
-            value={userDetails.rating}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-      <br />
-      <div>
-        {Employe.map((newValues) => {
-          return (
-            <div className="showDataStyle">
-              <p>Name : {newValues.username} | </p>
-              <p>Department : {newValues.department} |</p>
-              <p>rating : {newValues.rating} </p>
-            </div>
-          );
-        })}
-      </div>
+      )}
     </div>
   );
 };
